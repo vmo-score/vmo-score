720ec8225ff34fee85f5d696da05cb5420f55eab
Jaime Arias <jaime.arias@inria.fr>
2016-11-06 11:17:38 +0100

    updating documentation for gui_tk.py

--- VMO_Score/gui_tk.py
+++ VMO_Score/gui_tk.py
@@ -1,10 +1,13 @@
+"""Module containing the implementation of the graphical interface with Tk."""
+
 import Tkinter
 import tkFileDialog
 import tkMessageBox
 import ttk
-from PIL import Image
 
 import matplotlib
+from PIL import Image
+
 matplotlib.use('TkAgg')
 
 from segmentation import Segmentation
@@ -14,7 +17,8 @@ import utils
 
 class GraphicalInterface(Tkinter.Tk):
 
-    def initUI(self):
+    def init_ui(self):
+        """Initialization of the graphical interface"""
         # variables
         self.segmentation = None
         self.pn = None
@@ -31,6 +35,15 @@ class GraphicalInterface(Tkinter.Tk):
         self.resizable(False, False)
 
     def _create_segmentation_tab(self, nb):
+        """Add the frame and the widgets for the segmentation tab to a notebook.
+
+        Args:
+            nb: Notebook
+                Notebook to be updated.
+
+        Returns:
+            None
+        """
         frame = Tkinter.Frame(nb)
 
         # widgets for load audio file
@@ -38,7 +51,7 @@ class GraphicalInterface(Tkinter.Tk):
         audio_path_entry = Tkinter.Entry(frame, textvariable=self.audio_path)
         audio_path_entry.grid(column=0, row=0, sticky='EW',
                               columnspan=3)
-        audio_btn = Tkinter.Button(frame, text='Load', command=self.showDialog)
+        audio_btn = Tkinter.Button(frame, text='Load', command=self.show_dialog)
         audio_btn.grid(column=3, row=0, sticky='EW')
 
         self.output_path = Tkinter.StringVar()
@@ -46,7 +59,7 @@ class GraphicalInterface(Tkinter.Tk):
         output_path_entry.grid(column=0, row=1, sticky='EW',
                                columnspan=3)
         output_btn = Tkinter.Button(frame, text='Output',
-                                    command=lambda: self.showDialogFolder(1))
+                                    command=lambda: self.show_dialog_folder(1))
         output_btn.grid(column=3, row=1, sticky='EW')
 
         # buttons for actions
@@ -67,6 +80,15 @@ class GraphicalInterface(Tkinter.Tk):
         nb.add(frame, text='Segmentation')
 
     def _create_petri_tab(self, nb):
+        """Add the frame and the widgets for the petri net tab to a notebook.
+
+        Args:
+            nb: Notebook
+                Notebook to be updated.
+
+        Returns:
+            None
+        """
         frame = Tkinter.Frame(nb)
 
         for i in range(4):
@@ -77,7 +99,7 @@ class GraphicalInterface(Tkinter.Tk):
         pn_path_entry = Tkinter.Entry(frame, textvariable=self.pn_path)
         pn_path_entry.grid(column=0, row=0, sticky='EW', columnspan=3)
         pn_path_btn = Tkinter.Button(frame, text='Search',
-                                     command=lambda: self.showDialogFolder(2))
+                                     command=lambda: self.show_dialog_folder(2))
         pn_path_btn.grid(column=3, row=0, sticky='EW')
 
         # button to load the petri net
@@ -106,14 +128,27 @@ class GraphicalInterface(Tkinter.Tk):
     def __init__(self, parent):
         Tkinter.Tk.__init__(self, parent)
         self.parent = parent
-        self.initUI()
+        self.init_ui()
 
-    def showDialog(self):
+    def show_dialog(self):
+        """Show window to choose the audio path"""
         audio_path = tkFileDialog.askopenfilename(title="Open file")
         if audio_path:
             self.audio_path.set(audio_path)
 
-    def showDialogFolder(self, option):
+    def show_dialog_folder(self, option):
+        """Show a window to choose the output folder for the segmentation,
+        the oracle, the petri net, and the configuration.
+
+        Args:
+            option: int
+                If 1, set the output folder to save the segmentation and the
+                oracle. Otherwise, set the output folder only for the petri net
+                and the configuration.
+
+        Returns:
+            None
+        """
         folder_path = tkFileDialog.askdirectory(title="Output folder")
         if folder_path:
             if option == 1:
@@ -128,13 +163,36 @@ class GraphicalInterface(Tkinter.Tk):
             self.configuration_yml = folder_path + "/configuration.yml"
             self.petri_pnml = folder_path + "/petri-net"
 
-    def messageError(self, error_msg):
+    @staticmethod
+    def message_error(error_msg):
+        """Show an error message.
+
+        Args:
+            error_msg: str
+                Message to be displayed.
+
+        Returns:
+            None
+        """
         tkMessageBox.showerror('Message', error_msg)
 
-    def messageInfo(self, msg):
+    @staticmethod
+    def message_info(msg):
+        """Show a warning message
+
+        Args:
+            msg: str
+                Message to be displayed
+
+        Returns:
+            None
+        """
         tkMessageBox.showinfo('Message', msg)
 
     def petri_net(self):
+        """Create a Petri net from a segmentation analysis. The new net is
+        saved as a png, json, and pnml file. Moreover, a configuration file is
+        created in order to configure the petri net later."""
         if self.segmentation is not None:
             if self.output_path:
                 self.pn = PetriNet(s=self.segmentation)
@@ -143,67 +201,85 @@ class GraphicalInterface(Tkinter.Tk):
                 self.pn.to_pnml(self.petri_pnml)
                 utils.generate_configuration(self.configuration_yml, self.pn)
 
-                self.messageInfo("Petri Net created")
+                self.message_info("Petri Net created")
             else:
-                self.messageError("Set output path")
+                self.message_error("Set output path")
         else:
-            self.messageError("Missing segmentation")
+            self.message_error("Missing segmentation")
 
     def visualize_petri_net(self):
+        """Visualize the png image of the petri net."""
         if self.pn is not None and self.output_path:
             self.viewer(self.petri_png)
         else:
-            self.messageError("Missing Petri Net")
+            self.message_error("Missing Petri Net")
 
     def load_petri_net(self):
+        """Load a petri net from a pnml file."""
         if self.pn_path.get() != "":
             self.pn_prev = PetriNet(pnml=self.petri_pnml)
             self.pn_prev.output_png(self.petri_png_prev)
-            self.messageInfo("Petri Net loaded")
+            self.message_info("Petri Net loaded")
         else:
-            self.messageError("Set output path")
+            self.message_error("Set output path")
 
     def update_petri_net(self):
+        """Update the petri net from the configuration file."""
         if self.pn_prev is not None and self.pn_path.get() != "":
             self.pn_prev.update_from_config(self.configuration_yml)
             self.pn_prev.output_png(self.petri_png_prev)
-            self.messageInfo("Petri Net updated")
+            self.message_info("Petri Net updated")
         else:
-            self.messageError("Missing Petri Net")
+            self.message_error("Missing Petri Net")
 
     def preview_petri_net(self):
+        """Show an updated petri net."""
         if self.pn_prev is not None:
             self.viewer(self.petri_png_prev)
         else:
-            self.messageError("Missing Petri Net")
+            self.message_error("Missing Petri Net")
 
     def save_petri_net(self):
+        """Save the petri net as a json, pnml, and png file."""
         if self.pn_prev is not None and self.pn_path.get() != "":
             self.pn_prev.to_json(self.petri_json)
             self.pn_prev.to_pnml(self.petri_pnml)
             self.pn_prev.output_png(self.petri_png)
-            self.messageInfo("Petri Net saved")
+            self.message_info("Petri Net saved")
         else:
-            self.messageError("Missing Petri Net")
+            self.message_error("Missing Petri Net")
 
     def visualize_segmentation(self):
+        """Visualize the png image of the segmentation."""
         if self.segmentation is not None and self.output_path:
             self.viewer(self.segmentation_png)
         else:
-            self.messageError("Missing Segmentation")
+            self.message_error("Missing Segmentation")
 
     def create_segmentation(self):
+        """Generate the segmentation analysis from the audio file. The new
+        analysis is saved as a png file and also it is saved the oracle created
+        by vmo."""
         if self.audio_path.get():
             if self.output_path.get():
                 self.segmentation = Segmentation(self.audio_path.get())
                 self.segmentation.output_png(self.segmentation_png)
                 self.segmentation.save_oracle(self.oracle_path)
-                self.messageInfo("Segmentation done")
+                self.message_info("Segmentation done")
             else:
-                self.messageError("Set output path")
+                self.message_error("Set output path")
         else:
-            self.messageError("Set the audio file path")
+            self.message_error("Set the audio file path")
 
-    def viewer(self, path):
+    @staticmethod
+    def viewer(path):
+        """Visualize a png file
+
+        Args:
+            path: Path of the image to be visualized
+
+        Returns:
+            None
+        """
         load = Image.open(path)
         load.show()
