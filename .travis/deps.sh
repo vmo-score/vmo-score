b6b5b0507650c8f83c53db2cefff0f9d1551dc63
Jaime Arias <jaime.arias@inria.fr>
2017-04-12 09:39:33 +0200

    updating travis

--- .travis/deps.sh
+++ .travis/deps.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 curl -L http://www.mega-nerd.com/SRC/libsamplerate-0.1.8.tar.gz | tar xz
 (cd libsamplerate-0.1.8; ./configure; make; make install)
 
@@ -14,6 +16,10 @@ if [[ "$TRAVIS_OS_NAME" = "osx" ]]; then
 
   # make brew python the system default
   brew link --overwrite python
+
+  # install SIP for qt
+  curl -L https://sourceforge.net/projects/pyqt/files/sip/sip-4.19.2/sip-4.19.2.tar.gz | tar xz
+  (cd  sip-4.19.2; python configure.py; make; make install)
 
   pip install git+https://github.com/himito/macholib.git
   pip install setuptools==19.2
